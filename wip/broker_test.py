89c21366375d9a5d91adf4c756307aeca241ada6
willer <steve.willer@gmail.com>
2023-12-04 16:08:03 -0500

    lots of changes made in prod...

---
+++ wip/broker_test.py
@@ -0,0 +1,39 @@
+import redis, json
+import asyncio, datetime
+import sys
+import nest_asyncio
+import configparser
+import traceback
+from textmagic.rest import TextmagicRestClient
+
+nest_asyncio.apply()
+
+from broker_root import broker_root
+from broker_ibkr import broker_ibkr
+from broker_alpaca import broker_alpaca
+
+for account in ["U8438939", "PA3I5VZDCGPF"]:
+    print(f"Account: {account}")
+    bot = "live"
+    config = configparser.ConfigParser()
+
+    print("HEALTH CHECK")
+    config.read('config.ini')
+    aconfig = config[account]
+    if aconfig['driver'] == 'ibkr':
+        driver = broker_ibkr(bot, account)
+    elif aconfig['driver'] == 'alpaca':
+        driver = broker_alpaca(bot, account)
+    else:
+        raise Exception("Unknown driver: " + aconfig['driver'])
+    driver.health_check()
+
+    print("GET POSITION SIZE")
+    position_size = driver.get_position_size('ES1!')
+    print(f"Position size: {position_size}")
+
+    print("GET STOCK, CHECK FUTURES")
+    stock = driver.get_stock("ES1!")
+    print(f"Stock: {stock}, is_futures={stock.is_futures}")
+
+    print("")
---
+++ wip/webapp.py
@@ -0,0 +1,234 @@
+import json
+import redis, sqlite3, time, os, hashlib, math
+from flask import Flask, render_template, request, g, current_app
+
+app = Flask(__name__)
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+p = r.pubsub()
+p.subscribe('health')
+p.get_message(timeout=3)
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('trade.db')
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+# initial setup of db (if it doesn't exist)
+conn = sqlite3.connect('trade.db')
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS signals (
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+        ticker,
+        order_action,
+        order_contracts,
+        order_price,
+        order_message text
+    )
+""")
+conn.commit()
+
+# migrations for db, if you have older schemas
+cursor = conn.cursor()
+try:
+    cursor.execute("ALTER TABLE signals ADD COLUMN order_message text")
+    conn.commit()
+except: pass
+
+cursor = conn.cursor()
+try:
+    cursor.execute("ALTER TABLE signals ADD COLUMN bot text")
+    conn.commit()
+except: pass
+
+cursor = conn.cursor()
+try:
+    cursor.execute("ALTER TABLE signals ADD COLUMN market_position text")
+    conn.commit()
+except: pass
+
+cursor = conn.cursor()
+try:
+    cursor.execute("ALTER TABLE signals ADD COLUMN market_position_size text")
+    conn.commit()
+except: pass
+
+
+@app.context_processor
+def add_imports():
+    # Note: we only define the top-level module names!
+    return dict(hashlib=hashlib, time=time, os=os, math=math)
+
+## ROUTES
+
+# GET /
+@app.get('/')
+def dashboard():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT datetime(timestamp, 'localtime') as timestamp,
+        ticker,
+        bot,
+        order_action,
+        order_contracts,
+        market_position,
+        market_position_size,
+        order_price,
+        order_message
+        FROM signals
+        order by timestamp desc
+        LIMIT 500
+    """)
+    signals = cursor.fetchall()
+    #hashlib.sha1(row['order_message'])
+
+    return render_template('dashboard.html', signals=signals, sha1=hashlib.sha1)
+
+# POST /resend?hash=xxx
+@app.post('/resend')
+def resend():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT order_message
+        FROM signals
+        order by timestamp desc
+    """)
+    signals = cursor.fetchall()
+    for row in signals:
+        if request.form.get("hash") == hashlib.sha1(row["order_message"]).hexdigest():
+            r.publish('tradingview', row["order_message"])
+            return "<html><body>Found it!<br><br><a href=/>Back to Home</a></body></html>"
+    return "<html><body>Didn't find it!<br><br><a href=/>Back to Home</a></body></html>"
+
+# POST /order
+@app.post('/order')
+def order():
+    ticker = request.form.get("ticker")
+    pct = float(request.form.get("pct"))
+
+    # bot side only cares about a few fields
+    message = {
+        "ticker": ticker.upper(),
+        "strategy": {
+            "bot": "live",
+            "manual_pct": pct,
+        }
+    }
+    r.publish('tradingview', json.dumps(message))
+
+    return f"<html><body>Sent!<br>{message}<br><a href=/>Back to Home</a></body></html>"
+
+# POST /webhook
+@app.post("/webhook")
+def webhook():
+    data = request.data
+
+    if data:
+        r.publish('tradingview', data)
+
+        #print('got message: ' + request.get_data())
+
+        data_dict = request.json
+
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("""
+            INSERT INTO signals (ticker, bot, order_action, order_contracts, market_position, market_position_size, order_price, order_message) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """, (data_dict['ticker'], 
+                data_dict['strategy']['bot'],
+                data_dict['strategy']['order_action'], 
+                data_dict['strategy']['order_contracts'],
+                data_dict['strategy']['market_position'],
+                data_dict['strategy']['market_position_size'],
+                data_dict['strategy']['order_price'],
+                request.get_data()))
+        db.commit()
+
+        return data
+
+    return {"code": "success"}
+
+# GET /health
+@app.get("/health")
+def health():
+
+    # send a message to the redis channel to test connectivity
+    r.publish('tradingview', 'health check')
+    # check if we got a response 
+    message = p.get_message(timeout=15)
+    if message and message['type'] == 'message':
+        return {"code": "success"}
+
+    if message != None:
+        return {"code": "failure", "message-type": message['type'], "message": message['data']}, 500
+    else:
+        return {"code": "failure", "message-type": "timeout", "message": "no message received"}, 500
+
+# POST /stop-backend
+@app.post("/stop-backend")
+def stop_backend():
+    # find the broker processes and kill them
+    os.system("pkill -f start-broker-live.sh")
+    os.system("pkill -f broker.py")
+
+    return "<html><body>Done<br><br><a href=/>Back to Home</a></body></html>"
+
+# POST /start-backend
+@app.post("/start-backend")
+def start_backend():
+    # find the broker processes and kill them
+    os.system("pkill -f start-broker-live.sh")
+    os.system("pkill -f broker.py")
+
+    # start the broker processes in the background
+    if os.system("sh start-broker-live.sh &") != 0:
+        return "<html><body>Failed to start IBKR broker<br><br><a href=/>Back to Home</a></body></html>"
+
+    return "<html><body>Done<br><br><a href=/>Back to Home</a></body></html>"
+
+# GET /show-logs-ibkr?tail=xxx
+@app.get("/show-logs-ibkr")
+def show_logs_ibkr():
+    tail = request.args.get("tail")
+    if tail == None:
+        tail = 100
+    else:
+        tail = int(tail)
+
+    fname = "start-broker-ibkr-mac-live.sh.log"
+    if os.path.exists(fname):
+        with open(fname) as f:
+            # read the last n lines
+            lines = f.readlines()
+            lines = lines[-tail:]
+            lines = "".join(lines)
+            return f"<html><body><h1>IBKR Broker Logs</h1><br><br><a href=/>Back to Home</a><br><br><pre>{lines}</pre></body></html>"
+    else:
+        return "<html><body>File not found<br><br><a href=/>Back to Home</a></body></html>"
+
+# GET /show-logs-alpaca?tail=xxx
+@app.get("/show-logs-alpaca")
+def show_logs_alpaca():
+    tail = request.args.get("tail")
+    if tail == None:
+        tail = 100
+    else:
+        tail = int(tail)
+
+    fname = "start-broker-alpaca-mac.sh.log"
+    if os.path.exists(fname):
+        with open(fname) as f:
+            # read the last n lines
+            lines = f.readlines()
+            lines = lines[-tail:]
+            lines = "".join(lines)
+            return f"<html><body><h1>alpaca Broker Logs</h1><br><br><a href=/>Back to Home</a><br><br><pre>{lines}</pre></body></html>"
+    else:
+        return "<html><body>File not found<br><br><a href=/>Back to Home</a></body></html>"
+
